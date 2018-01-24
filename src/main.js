8f29f7f6bc0adc170e5ee1b84ca23b1ae5993cec
Glenn Gijsberts <glenngijsberts@MacBook-Pro-van-Glenn.local>
2018-01-24 15:07:51 +0100

    Added vue router to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter);
+
+//Components
+import App from './App.vue'
+import Home from './components/Home.vue'
+
+//Routes
+const routes = [
+  { path: '/', name: 'home', component: Home }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+});
+
+//Vue Init
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
